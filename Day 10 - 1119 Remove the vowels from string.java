57d38c42779ba1070f0ff903f2a05366c18187f5
smansura5408-syed <smansura5408@gmail.com>
2026-07-23 09:54:47 +0530

    Create Day 10 - 1119 Remove the vowels from string.java

---
+++ Day 10 - 1119 Remove the vowels from string.java
@@ -0,0 +1,20 @@
+public class Main {
+
+public static void main(string[] args) { 
+    String s = "leetcodeisacommunityforcoders";
+
+StringBuilder sb = new StringBuilder();
+
+for(char c: s.tocharArray()) {
+
+if (c != 'a' && c != e' && c != 'i' && c != 'o' && c != 'u') {
+
+sb.append(c);
+
+}
+}
+
+System.out.println("Input: " + s);
+System.out.println("Output: "+ sb.tostring());
+}
+}
